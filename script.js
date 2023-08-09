2c9c646b9c0aec4d52fd37a0ecb4f6f741d508ed
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-09 19:09:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
    let r=parseFloat(document.getElementById("radius").value);
     let v = (4/3)*Math.PI*Math.pow(r,3);
-    document.getElementById("volume").value=v.toFixed(2);
+    document.getElementById("volume").value=v.toFixed(4);
 	return false
 } 
 
